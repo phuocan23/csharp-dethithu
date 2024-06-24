c1e6bfec4db4d23b26c287daf7ecf0b9f07f4393
ADMIN <ADMIN@LAB1-MAY22>
2024-06-24 16:53:07 +0700

    Add project files.

---
+++ định dạng.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace csharp_dethithu
+{
+    public partial class định_dạng : Form
+    {
+        public định_dạng()
+        {
+            InitializeComponent();
+        }
+    }
+}
